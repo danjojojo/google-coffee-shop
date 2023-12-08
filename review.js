9763d09d71bfec1c4f896376c223a7893117ade1
Daniel <152934075+danjojojo@users.noreply.github.com>
2023-12-08 18:07:24 +0000

    Applied visual designs, gave more responsiveness factor

--- review.js
+++ review.js
@@ -4,19 +4,16 @@ let slideIndex = 0;
 function loadReviews(review){
     return`
     <div class="review">
-        <p class="review__profile">${review.username[0]}</p>
+        <div class="profile-pic" style="background-color: ${review.headshotcolor}; >
+            <p class="review__profile">${review.username[0]}</p>
+        </div>
         <p class="review__name">${review.username}</p>
         <p class="review__body">${review.body}</p>
     </div>
     `;
 }
 
-function getFirstLetter(username){
-    console.log(username[0]);
-}
-
 function moveSlider(e){
-
     if(e.currentTarget.id.includes('right')){
         slideIndex === reviews.length - 1 ? (slideIndex = 0) : slideIndex++;
     } else {
@@ -47,11 +44,51 @@ async function fetchReviews(){
 
 fetchReviews();
 
+
+
 // 3. Add event listeners to move the sliders left and right
 
 document.querySelector('#arrow--right').addEventListener('click', moveSlider);
 document.querySelector('#arrow--left').addEventListener('click', moveSlider);
+document.querySelector('.add-to-order').onclick = addtoorder;
+
+function addtoorder(){
+    alert(`Added to cart successfully!`);
+}
 
 
-// Mema
+// FOR BEST SELLERS
+// let bestsellers;
+
+
+// function loadBestSellers(bestsellers){
+//     return`
+//     <div class="review">
+//         <div class="profile-pic">
+//             <p class="review__profile">${review.username[0]}</p>
+//         </div>
+//         <p class="review__name">${review.username}</p>
+//         <p class="review__body">${review.body}</p>
+//     </div>
+//     `;
+// }
+
+// async function fetchBestSellers(){
+//     await fetch('bestsellers.json')
+//     .then((response) => {
+//         if(!response.ok){
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         bestsellers = data;
+//         // 2. Parse the data and create the 'review' divs
+//         document.querySelector('.best-sellers').innerHTML = data.map(loadBestSellers).join('');
+//     })
+//     .catch((error) => {
+//         console.error('There has been a problem with your fetch operation', error);
+//     });
+// }
+
 
